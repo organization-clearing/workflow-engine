07bdc4dde70de15ababebd504bc68a1b7f574ac7
hong <wanli112358@163.com>
2018-12-10 17:13:42 +0800

    部分xml配置

--- workflow-dispatcher/src/main/java/org/clearing/workflow/dispatcher/test/DubboTest.java
+++ workflow-dispatcher/src/main/java/org/clearing/workflow/dispatcher/test/DubboTest.java
@@ -1,15 +1,10 @@
 package org.clearing.workflow.dispatcher.test;
 
-import java.util.HashMap;
-
-import org.clearing.frame.rpc.server.RPCServer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DubboTest {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		RPCServer rpcServer = (RPCServer) context.getBean("rpcServer");
-		System.out.println(rpcServer.doServer(null ));
-		System.out.println(rpcServer.doServer(new HashMap<>()).getResult());
+		System.out.println(context.getBean("dataSource"));
 	}
 }
