24138857f7f0eb1a3c53ec2c8e22aa3427ff9ecf
hong <wanli112358@163.com>
2018-12-10 17:28:26 +0800

    s

--- workflow-dispatcher/src/main/java/org/clearing/workflow/dispatcher/test/DubboTest.java
+++ workflow-dispatcher/src/main/java/org/clearing/workflow/dispatcher/test/DubboTest.java
@@ -5,6 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DubboTest {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		System.out.println(context.getBean("dataSource"));
+//		System.out.println(context.getBean("dataSource"));		
 	}
 }
